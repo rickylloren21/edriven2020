3604e872c0bd5868a99b968cd33377fe1a045ac2
ricky <rickylloren@icloud.com>
2021-02-04 17:15:53 +0800

    Inititalize Repo

---
+++ js/my.js
@@ -0,0 +1,39 @@
+console.log("included");
+
+let tweetobj = {
+    profilename: "Design Bolts",
+    username: "@designbolts",
+    tweet: "I have a new resource roundup out today",
+    retweets: 6,
+    likes: 18,
+    isShown: true,
+    replies: [
+        {
+            username:"@juliusceasar",
+            reply:"This actually nice",
+        },
+        {
+            username:"@johnricky",
+            reply:"This actually nice",
+        },
+        {
+            username:"@designbolts",
+            reply:"thank you all",
+        }
+    ],
+    retweet: function () {
+        console.log("retweeting");
+    },
+    hitLike: function () {
+        //console.log(this.likes++);
+        this.likes++
+    },
+    showReplies: function() {
+        for(reply of this.replies){
+            console.log(`${reply.username}: ${reply.reply}`);
+        }
+    }
+}
+tweetobj.showReplies();
+console.log(tweetobj);
+//console.log(tweetobj.likes);
