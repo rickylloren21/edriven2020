b716806c2eb96b8657ebf5856a137a80fa41fb2a
ricky <rickylloren@icloud.com>
2021-02-23 22:21:47 +0800

    initialize repo

--- js/my.js
+++ js/my.js
@@ -1,39 +1,8 @@
-console.log("included");
-
-let tweetobj = {
-    profilename: "Design Bolts",
-    username: "@designbolts",
-    tweet: "I have a new resource roundup out today",
-    retweets: 6,
-    likes: 18,
-    isShown: true,
-    replies: [
-        {
-            username: "@juliusceasar",
-            reply:"This actually nice",
-        },
-        {
-            username: "@johnricky",
-            reply:"This actually nice",
-        },
-        {
-            username: "@designbolts",
-            reply:"thank you all",
-        }
-    ],
-    retweet: function () {
-        console.log("retweeting");
-    },
-    hitLike: function () {
-        //console.log(this.likes++);
-        this.likes++
-    },
-    showReplies: function () {
-        for (reply of this.replies) {
-            console.log(`${reply.username}: ${reply.reply}`);
-        }
+function Person(usern, age, gend){
+    this.username = usern;
+    this.age = ag;
+    this.gender = gend;
+    this.getInfro = function (){
+        return $ name is;
     }
 }
-tweetobj.showReplies();
-//console.log(tweetobj);
-//console.log(tweetobj.likes);
